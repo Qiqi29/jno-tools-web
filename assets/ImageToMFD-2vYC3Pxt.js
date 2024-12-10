ae932182224dcc6f2a6777c56727c6e47a637fd0
Qii <112358908+Qiqi29@users.noreply.github.com>
2024-12-10 16:22:19 +0800

    修改的有点多，懒得写了

---
+++ assets/ImageToMFD-2vYC3Pxt.js
@@ -0,0 +1,1 @@
+import{t as a}from"./Topbar-C12HIlMK.js";import{c as o,b as s,a as r,o as i}from"./index-CdybBFZv.js";import"./Icon-_bBmy0Zw.js";const l={class:"imagetomfd-page"},d={__name:"ImageToMFD",setup(m){return(t,e)=>(i(),o("div",l,[s(a,{title:t.$t("imageToMFD.title")},null,8,["title"]),e[0]||(e[0]=r("div",{class:"container"},null,-1))]))}};export{d as default};
