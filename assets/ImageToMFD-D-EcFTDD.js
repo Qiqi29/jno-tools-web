cfc004856239961b63143826f6e53db8f6180c3c
Qii <112358908+Qiqi29@users.noreply.github.com>
2024-12-11 15:05:42 +0800

    优化了很多东西
    优化弹窗组件，优化手动复制逻辑

---
+++ assets/ImageToMFD-D-EcFTDD.js
@@ -0,0 +1,1 @@
+import{t as a}from"./Topbar-CPZzBfPl.js";import{c as o,b as s,a as r,o as i}from"./index-DxQsIYMw.js";import"./Icon-AMVm_MOg.js";const l={class:"imagetomfd-page"},d={__name:"ImageToMFD",setup(m){return(t,e)=>(i(),o("div",l,[s(a,{title:t.$t("imageToMFD.title")},null,8,["title"]),e[0]||(e[0]=r("div",{class:"container"},null,-1))]))}};export{d as default};
