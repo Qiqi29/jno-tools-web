259676004315f601e59522e76bbbe8844bec7743
Qii <112358908+Qiqi29@users.noreply.github.com>
2024-12-14 16:34:56 +0800

    新增 Vizzy 文档

---
+++ assets/ImageToMFD-BaWqyL9y.js
@@ -0,0 +1,1 @@
+import{Q as a}from"./Topbar-RxmTMtWn.js";import{c as o,b as s,a as r,o as i}from"./index-SdcPHFtT.js";import"./Icon-CwC--XFN.js";const l={class:"imagetomfd-page"},d={__name:"ImageToMFD",setup(m){return(t,e)=>(i(),o("div",l,[s(a,{title:t.$t("imageToMFD.title")},null,8,["title"]),e[0]||(e[0]=r("div",{class:"container"},null,-1))]))}};export{d as default};
