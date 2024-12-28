d2667751c0ba05b743c76e9aae5800aee5eb793f
Qii <112358908+Qiqi29@users.noreply.github.com>
2024-12-28 16:44:51 +0800

    新增作品编辑器（测试）

---
+++ assets/ImageToMFD-Doc-zqNj.js
@@ -0,0 +1,1 @@
+import{Q as a}from"./Topbar-BWAsgqvs.js";import{c as o,b as s,a as r,o as i}from"./index-C18C_xQ6.js";import"./Icon-rIM7ThDM.js";const l={class:"imagetomfd-page"},d={__name:"ImageToMFD",setup(m){return(t,e)=>(i(),o("div",l,[s(a,{title:t.$t("imageToMFD.title")},null,8,["title"]),e[0]||(e[0]=r("div",{class:"container"},null,-1))]))}};export{d as default};
